eaee00e52f13421a1d39022e8ecb389f87e6fcbf
aniruddha-chunne <achunne3@gmail.com>
2023-11-05 07:26:11 +0530

    First Commit of OCJA Exam

---
+++ src/main/java/com/OCJA/OcjaApplication.java
@@ -0,0 +1,16 @@
+package com.OCJA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcjaApplication.class, args);
+
+		System.out.println("OCJA Application Starts here");
+
+	}
+
+}
---
+++ src/test/java/com/OCJA/OcjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OCJA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
