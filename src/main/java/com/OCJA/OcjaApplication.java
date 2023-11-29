cb9266aee1851dacad2e88a7f12505fb5d661616
aniruddha_chunne <aniruddha_chunne@calypso.com>
2023-11-29 10:32:16 +0530

    add a tutorials
    Change-Id: I8079e362ad8c6d4a28419b6e2b5b93b73b2df7c1

--- src/main/java/com/OCJA/OcjaApplication.java
+++ src/main/java/com/OCJA/OcjaApplication.java
@@ -9,6 +9,14 @@ public class OcjaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OcjaApplication.class, args);
 		System.out.println("OCJA Application Starts here");
+
+		int number  = 10;
+		int Number  = 20;
+		int Runnable = 30;
+
+		System.out.println(number);
+		System.out.println(Number);
+		System.out.println(Runnable);
 	}
 
 }
