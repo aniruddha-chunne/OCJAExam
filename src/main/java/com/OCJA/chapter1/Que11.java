51649c37617bc4c409ba2e6c1380e9c9b43ae361
aniruddha_chunne <aniruddha_chunne@calypso.com>
2023-11-30 17:13:53 +0530

    chaper 1 - 11, 12
    Change-Id: I6f2a75ba9a99eab17b68eaf963c345f50a227338

---
+++ src/main/java/com/OCJA/chapter1/Que11.java
@@ -0,0 +1,38 @@
+package com.OCJA.chapter1;
+
+import java.util.*;
+import java.util.*;
+
+public class Que11 {
+
+    /*
+
+    /37 - Java Basics
+    //522 - Answer
+
+    11. Which statement about import statements is true?
+    A. The class will not compile if it contains unused import
+    statements.
+
+    B. Unused import statements can be removed from the class
+    without causing a class to become unable to be compiled.
+
+  *******  C. The class will not compile if a duplicate import statement is
+    present.
+
+    D. If a class contains an import statement for a class used in
+    the program that cannot be found, it can still compile.
+
+    11. B. A class will compile if it has unused or redundant import
+    statements, making Option A and C incorrect. Option D is also
+    incorrect as the compiler must be able to locate the class of the
+    import statement. The correct answer is Option B. Removing
+    unused import statements does not cause a class to become
+    unable to be compiled.
+
+    */
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/OCJA/chapter1/Que12.java
@@ -0,0 +1,27 @@
+package com.OCJA.chapter1;
+
+/*
+
+        12. What is the result of compiling and executing the following
+        class?
+        1: public class ParkRanger {
+        2: int birds = 10;
+        3: public static void main(String[] data) {
+        4: int trees = 5;
+        5: System.out.print(trees+birds);
+        6:    }
+        7: }
+
+        A. It does not compile.
+        B. It compiles but throws an exception at runtime.
+        C. It compiles and outputs 5.
+        D. It compiles and outputs 15.
+
+*/
+
+public class Que12
+{
+    public static void main(String[] args) {
+
+    }
+}
